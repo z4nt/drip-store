7af70432dd4abff1a932bc0361cb81137faf6ae7
ANTONIEL FERREIRA SOUSA <ANTONIEL0867@DIGITALCOLLEGE.LOCAL>
2024-07-20 15:56:28 -0300

    a

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,19 +6,19 @@ import '../index.css'
 export default function Nav() {
     return (
         <nav className="flex flex-col border">
-    <div className="px-[200px] items-center py-6 flex justify-between">
+    <div className="px-[200px] items-center py-6 flex justify-between sm:p-0">
         <div className="">
             <img src="./images/logo.svg" alt="logo da digital college"/>
         </div>
 
         <form className="max-w-md mx-auto">
             <div className="relative">
-              <div className="absolute inset-y-0 end-0 flex items-center ps-3 pointer-events-none">
+              <div className="sm:mr-4 absolute inset-y-0 end-0 flex items-center ps-3 pointer-events-none">
                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
               </div>
-              <input type="search" id="default-search" class="top-0 left-0 block w-[500px] p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pesquisar produto..." required />
+              <input type="search" id="default-search" class="sm:w-60 top-0 left-0 block w-[500px] p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pesquisar produto..." required />
             </div>
         </form>
 
